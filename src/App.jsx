56e250e200edd6a3ee8035526a19d164f07003e9
Oliver Y. Kui <oliverkui0324@outlook.com>
2024-01-16 21:19:45 +0800

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import Dilution from "./Dilution";
-import FindConcentration from "./FindConcentration";
-import FindMass from "./FindMass";
-import FindVolume from "./FindVolume";
+import { Outlet, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="h-screen flex flex-col gap-4">
-      <header className="border-b-2 font-bold text-4xl py-2 px-5">
-        MoleCal
+      <header className="border-b-2 py-2 px-5 flex gap-20">
+        <div className="font-bold text-4xl flex flex-col justify-center">
+          <div>MoleCal</div>
+        </div>
+        <div className="flex gap-7 text-xl font-bold">
+          <div className="flex flex-col justify-center">
+            <Link to="/findMass">
+              <div className="border-black hover:border-b-2">Find Mass</div>
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center">
+            <Link to="/findConcentration">
+              <div className="border-black hover:border-b-2">
+                Find Concentration
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center">
+            <Link to="/findVolume">
+              <div className="border-black hover:border-b-2">Find Volume</div>
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center">
+            <Link to="/dilution">
+              <div className="border-black hover:border-b-2">Dilution</div>
+            </Link>
+          </div>
+        </div>
       </header>
-      <main className="flex flex-wrap gap-10 justify-center">
-        <FindMass />
-        <FindVolume />
-        <FindConcentration />
-        <Dilution />
+      <main className="flex p-10 justify-center">
+        <Outlet />
       </main>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Dilution from "./Dilution.jsx";
+import FindMass from "./FindMass.jsx";
+import FindVolume from "./FindVolume.jsx";
+import FindConcentration from "./FindConcentration.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <FindMass />,
+      },
+      {
+        path: "/findMass",
+        element: <FindMass />,
+      },
+      {
+        path: "/findConcentration",
+        element: <FindConcentration />,
+      },
+      {
+        path: "/findVolume",
+        element: <FindVolume />,
+      },
+      {
+        path: "/dilution",
+        element: <Dilution />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
